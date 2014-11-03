002da00330f870fc71612120936e73d25b1bcd34
Patrick Grasso <pgrasso@stevens.edu>
2014-11-03 00:43:36 -0500

    Add compare & group algorithm to find possible schedules given class section names (without section letter) e.g. 'MA 222'. Also, add some temporary test methods to make sure thistuff works

--- js/scheduler.js
+++ js/scheduler.js
@@ -26,12 +26,64 @@ var SCHEDULER = (function () {
             wsPattern   = new RegExp("[A-Z]{1,4} ?[0-9]{3}W[S01]"),
             labPattern  = new RegExp("[A-Z]{1,4} ?[0-9]{3}L[A-Z]$");
 
+
+        /**
+         * Time object constructor
+         */
+        function Time(timeStr) {
+            if (timeStr === null || timeStr === undefined) {
+                this.hour = null;
+                this.minute = null;
+            } else {
+                this.hour   = parseInt(timeStr, 10);
+                this.minute = parseInt(timeStr.slice(2), 10) || parseInt(timeStr.slice(3), 10);
+            }
+
+            // Returns minute difference between otherTime and this time
+            this.compareTo = function (otherTime) {
+                if (otherTime === null || otherTime.hour === null) {
+                    return 0;
+                }
+                return ((this.hour * 60) + this.minute) - ((otherTime.hour * 60) + otherTime.minute);
+            };
+        }
+
+
+        /**
+         * Meeting object constructor
+         */
+        function Meeting(xmlObj, day) {
+            this.day        = day;
+            this.starttime  = new Time(xmlObj.getAttribute("StartTime"));
+            this.endtime    = new Time(xmlObj.getAttribute("EndTime"));
+            this.site       = xmlObj.getAttribute("Site"); // This may be unnecessary
+            this.building   = xmlObj.getAttribute("Building");
+            this.room       = xmlObj.getAttribute("Room");
+            this.activity   = xmlObj.getAttribute("Activity");
+
+            this.checkConflict = function (otherMeeting) {
+                if (otherMeeting.day !== this.day || otherMeeting.starttime.hour === null) {
+                    return false;
+                }
+                if (otherMeeting.starttime.compareTo(this.starttime) >= 0 &&
+                           this.endtime.compareTo(otherMeeting.starttime) >= 0) {
+                    return true;
+                }
+                if (this.starttime.compareTo(otherMeeting.starttime) >= 0 &&
+                           otherMeeting.endtime.compareTo(this.starttime) >= 0) {
+                    return true;
+                }
+                return false;
+            };
+        }
+
+
         /**
          * Course object constructor
          */
-        function Course(id_, title_) {
-            this.id = id_;
-            this.title = title_;
+        function Course(id, title) {
+            this.id = id;
+            this.title = title;
             this.lectures = [];
             this.recitations = [];
             this.workshops = [];
@@ -46,21 +98,22 @@ var SCHEDULER = (function () {
                     this.workshops.push(sectionObj);
                 } else if (labPattern.test(sectionObj.section)) {
                     this.labs.push(sectionObj);
-                } else {
+                }/* else {
                     console.log("Unidentified Class Type: " + sectionObj.section);
                     console.log(sectionObj);
-                }
+                }*/
             };
         }
+
 
         /**
          * Section object constructor
          * Use for lectures, recitations, workshops, and labs
          */
-        function Section(xmlObj, type_) {
+        function Section(xmlObj, type) {
             var i, j, d, meetingObjs;
 
-            this.type           = type_;
+            this.type           = type;
 
             // Get values from XML tag
             this.section        = xmlObj.getAttribute("Section");
@@ -83,64 +136,88 @@ var SCHEDULER = (function () {
                 d = meetingObjs[i].getAttribute("Day");
 
                 if (d === "TBA") {
-                    this.meetings.push(new Meeting(meetingObjs[i], d))
+                    this.meetings.push(new Meeting(meetingObjs[i], d));
                 }
                 for (j = 0; j < d.length; j += 1) {
                     this.meetings.push(new Meeting(meetingObjs[i], d[j]));
                 }
             }
-        }
 
-        /**
-         * Meeting object constructor
-         */
-        function Meeting(xmlObj, day_) {
-            this.day        = day_;
-            this.starttime  = new Time(xmlObj.getAttribute("StartTime"));
-            this.endtime    = new Time(xmlObj.getAttribute("EndTime"));
-            this.site       = xmlObj.getAttribute("Site"); // This may be unnecessary
-            this.building   = xmlObj.getAttribute("Building");
-            this.room       = xmlObj.getAttribute("Room");
-            this.activity   = xmlObj.getAttribute("Activity");
+            this.checkConflict = function (otherSection) {
+                var i, j;
 
-            this.checkConflict = function (otherMeeting) {
-                if (otherMeeting.day !== this.day || otherMeeting.starttime.hour === null) {
-                    return false;
-                } else if (otherMeeting.starttime.compareTo(this.starttime) >= 0 &&
-                           this.endtime.compareTo(otherMeeting.starttime) >= 0) {
-                    return true;
-                } else if (this.starttime.compareTo(otherMeeting.starttime) >= 0 &&
-                           otherMeeting.endtime.compareTo(this.starttime) >= 0) {
-                    return true;
+                for (i = 0; i < this.meetings.length; i += 1) {
+                    for (j = 0; j < otherSection.meetings.length; j += 1) {
+                        if (this.meetings[i].checkConflict(otherSection.meetings[j])) {
+                            return true;
+                        }
+                    }
                 }
                 return false;
             };
         }
 
-        scheduler.Meeting = Meeting;
 
-        /**
-         * Time object constructor
-         */
-        function Time(timeStr) {
-            if (timeStr === null || timeStr === undefined) {
-                this.hour = null;
-                this.minute = null;
-            } else {
-                this.hour   = parseInt(timeStr);
-                this.minute = parseInt(timeStr.slice(2)) || parseInt(timeStr.slice(3));
-            }
+        function getValidSchedules(schedule, course) {
+            var i, j, k, valid, tempSchedules, newSchedules;
 
-            // Returns minute difference between otherTime and this time
-            this.compareTo = function (otherTime) {
-                if (otherTime === null || otherTime.hour === null) {
-                    return 0;
+            // Go through each lecture in the course. If the lecture fits into a
+            // schedule (doesn't conflict with ANY section in the schedule), copy
+            // the schedule, push the lecture section, and push it to newSchedules
+            newSchedules = balaclava(schedule, course.lectures);
+
+            if (course.recitations.length > 0) {
+                for (i = 0, tempSchedules = []; i < newSchedules.length; i += 1) {
+                    tempSchedules = tempSchedules.concat(balaclava(newSchedules[i], course.recitations));
                 }
-                return ((this.hour * 60) + this.minute) - ((otherTime.hour * 60) + otherTime.minute);
+                newSchedules = tempSchedules;
             }
+            if (course.workshops.length > 0) {
+                for (i = 0, tempSchedules = []; i < newSchedules.length; i += 1) {
+                    tempSchedules = tempSchedules.concat(balaclava(newSchedules[i], course.workshops));
+                }
+                newSchedules = tempSchedules;
+            }
+            if (course.labs.length > 0) {
+                for (i = 0, tempSchedules = []; i < newSchedules.length; i += 1) {
+                    tempSchedules = tempSchedules.concat(balaclava(newSchedules[i], course.labs));
+                }
+                newSchedules = tempSchedules;
+            }
+            return newSchedules;
         }
 
-        scheduler.Time = Time;
+        function balaclava(schedule, sections) {
+            var i, j, valid, tempSchedule, newSchedules = [];
+
+            for (i = 0, valid = false; i < sections.length; i += 1) {
+                for (j = 0, valid = true; j < schedule.length; j += 1) {
+                    valid &= !schedule[j].checkConflict(sections[i]);
+                }
+                if (valid) {
+                    tempSchedule = schedule.slice(0);
+                    tempSchedule.push(sections[i]);
+                    newSchedules.push(tempSchedule);
+                }
+            }
+            return newSchedules;
+        }
+
+        function mash(scheduleList, course) {
+            var i, newSchedules = [];
+
+            if (course === null || course === undefined) {
+                return scheduleList;
+            }
+            for (i = 0; i < scheduleList.length; i += 1) {
+                newSchedules = newSchedules.concat(getValidSchedules(scheduleList[i], course));
+            }
+            return newSchedules;
+        }
+
+        scheduler.mash = mash;
+
+
 
         /**
          * Parses the XML Course tags, grouping different sections together for a course
@@ -163,19 +240,40 @@ var SCHEDULER = (function () {
                 for (j = 0; j < sections.length; j += 1) {
                     if (sections[j].section.match(idPattern)[0] === course.id) {
                         course.addSection(sections.splice(j, 1)[0]);
+                        i -= (i === j) ? 1 : 0;
                         j -= 1;
                     }
                 }
                 courses.push(course);
             }
+            test();
         }
 
-        
-        scheduler.find = function(id) {
+
+        scheduler.find = function (id) {
             var i;
             for (i = 0; i < courses.length; i += 1) {
                 if (courses[i].id.indexOf(id) !== -1) {
-                    console.log(courses[i]);
+                    return courses[i];
+                }
+            }
+        };
+
+        scheduler.deepFind = function (id) {
+            var i, j;
+            for (i = 0; i < courses.length; i += 1) {
+                if (courses[i].id.indexOf(id) !== -1) {
+                    return courses[i];
+                }
+                for (j = 0; j < courses[i].lectures.length; j += 1) {
+                    if (courses[i].lectures[j].section.indexOf(id) !== -1) {
+                        return courses[i];
+                    }
+                }
+                for (j = 0; j < courses[i].recitations.length; j += 1) {
+                    if (courses[i].recitations[j].section.indexOf(id) !== -1) {
+                        return courses[i];
+                    }
                 }
             }
         };
@@ -188,7 +286,7 @@ var SCHEDULER = (function () {
 
 
         scheduler.init = function () {
-            var fileToGet = '2015S-' + (new Date()).toISOString().slice(0, 10) + '.xml';
+            var fileToGet = '2015S-' + (new Date()).toLocaleDateString().replace(/\//g, '-') + '.xml';
 
             if (window.XMLHttpRequest) {
                 req = new XMLHttpRequest();
@@ -224,12 +322,12 @@ var SCHEDULER = (function () {
 
             for (i = 0; i < courses.length; i += 1) {
                 if (courses[i].id.indexOf(query) >= 0 ||
-                    courses[i].title.toUpperCase().indexOf(query) >= 0) {
-                    tr = document.createElement("tr"),
-                    td = document.createElement("td"),
-                    td.appendChild(document.createTextNode(courses[i].id + " - " + courses[i].title));
+                        courses[i].title.toUpperCase().indexOf(query) >= 0) {
+                    tr = document.createElement("tr");
+                    td = document.createElement("td");
+                    td.appendChild(document.createTextNode(courses[i].id));
                     tr.appendChild(td);
-                    tr.addEventListener('click', function(event) {
+                    tr.addEventListener('click', function (event) {
                         document.getElementById("course_input").value = event.target.innerText;
                         while (suggestionBox.firstChild) {
                             suggestionBox.removeChild(suggestionBox.firstChild);
@@ -248,7 +346,7 @@ var SCHEDULER = (function () {
             if (query === '' || query.trim() === '') {
                 return;
             }
-            
+
             this.search(query.toUpperCase());
         };
 
@@ -264,3 +362,31 @@ window.onkeydown = function (event) {
         SCHEDULER.addCourse();
     }
 };
+
+
+// TEMPORARY ~~ TESTING //
+var x, y, z;
+
+function test() {
+    z = SCHEDULER.find('MA 123');
+    y = [[z.lectures[0], z.recitations[0]]];
+    x = SCHEDULER.mash(y, SCHEDULER.find('CS 385'));
+}
+
+function makeMySchedule(courseArray) {
+    var i, schedules = [[]];
+
+    for (i = 0; i <= courseArray.length; i += 1) {
+        schedules = SCHEDULER.mash(schedules, SCHEDULER.find(courseArray[i]));
+    }
+    return schedules;
+}
+
+function craftLink(schedule) {
+    var i, link = "http://web.stevens.edu/scheduler/#2015S=";
+
+    for (i = 0; i < schedule.length; i += 1) {
+        link += schedule[i].callnumber + ',';
+    }
+    return link;
+}
