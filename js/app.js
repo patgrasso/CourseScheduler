dd45e92a0aa96eff4fc005d24d794c9287b50b80
Patrick Grasso <pgrasso@stevens.edu>
2014-11-05 02:10:14 -0500

    Styling for data fields, more UI work done with angular

--- js/app.js
+++ js/app.js
@@ -2,12 +2,20 @@
 	var app = angular.module('scheduler', [ ]);
 
 
-	app.controller('CourseSelectController', function () {
+	app.controller('ScheduleController', function () {
 		this.searchResults = SCHEDULER.courses;
 		this.query = "";
+		this.current = this.searchResults[0];
+		this.currentSection = null;
 
 		this.updateSearchResults = function () {
 			this.searchResults = SCHEDULER.search(this.query.toUpperCase());
+			this.current = this.searchResults[0];
+			this.currentSection = this.searchResults[0].lectures[0];
+		};
+
+		this.updateRecitations = function () {
+			this.currentSection = this.currentSelection.lectures[0];
 		};
 	});
 })();
